842a730614f7603be21ced64e0bb7a0a723d7f66
Bruna Campos <92brunacampos@gmail.com>
2022-04-03 20:20:42 -0300

    base About Component

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,9 +1,47 @@
 import React from 'react';
 import './about.css';
+import { IoMdSchool } from 'react-icons/io';
+import { AiFillFolderOpen } from 'react-icons/ai';
+import { FaAward } from 'react-icons/fa';
+import ME from '../../assets/foto_perfil.png';
 
 function About() {
   return (
-    <section id="about">About</section>
+    <section id="about">
+      <h5>Get To Know</h5>
+      <h2>About Me</h2>
+
+      <div className="container about__container">
+        <div className="about__me">
+          <div className="about__me-image">
+            <img src={ME} alt="about me" />
+          </div>
+        </div>
+
+        <div className="about__content">
+          <div className="about__cards">
+
+            <article className="about__card">
+              <IoMdSchool className="about__icon" />
+              <h5>Education</h5>
+              <small>01/2022 Bachelor of Science - BS | Computer Science</small>
+            </article>
+
+            <article className="about__card">
+              <FaAward className="about__icon" />
+              <h5>Experience</h5>
+              <small>Software Developer Intern | Iteris</small>
+            </article>
+
+            <article className="about__card">
+              <AiFillFolderOpen className="about__icon" />
+              <h5>Portfolio</h5>
+              <small>+30 Projects</small>
+            </article>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 }
 
