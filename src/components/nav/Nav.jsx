16afeaf71f3c7d23a4ca774efa37805b7ab97b51
Bruna Campos <92brunacampos@gmail.com>
2022-04-03 19:11:25 -0300

    floating nav component, css and useState Hook

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,9 +1,22 @@
-import React from 'react';
+/* eslint-disable jsx-a11y/anchor-is-valid */
+/* eslint-disable jsx-a11y/control-has-associated-label */
+import React, { useState } from 'react';
 import './nav.css';
+import { AiOutlineHome, AiOutlineUser } from 'react-icons/ai';
+import { BiMessageSquareDetail } from 'react-icons/bi';
+import { BsLaptop } from 'react-icons/bs';
+import { RiServiceLine } from 'react-icons/ri';
 
 function Nav() {
+  const [activeNav, setActiveNav] = useState('#');
   return (
-    <div>Nav</div>
+    <nav>
+      <a href="#" onClick={() => setActiveNav('#')} className={activeNav === '#' ? 'active' : ''}><AiOutlineHome /></a>
+      <a href="#about" onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><AiOutlineUser /></a>
+      <a href="#experience" onClick={() => setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ''}><BsLaptop /></a>
+      <a href="#portfolio" onClick={() => setActiveNav('#portfolio')} className={activeNav === '#portfolio' ? 'active' : ''}><RiServiceLine /></a>
+      <a href="#contact" onClick={() => setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}><BiMessageSquareDetail /></a>
+    </nav>
   );
 }
 
